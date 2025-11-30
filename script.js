750c6b12185d90f2134425e6d6c3b8a2237d25c2
Aitaneuh <ethanb9407@gmail.com>
2025-11-30 02:40:33 +0100

    min and max values are now customizable as well as delta

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ import { draw_plot } from "./draw_plot.js"
 
 document.getElementById("plotBtn").addEventListener("click", () => {
     const expr = document.getElementById("func").value;
+    const minVal = parseFloat(document.getElementById("minVal").value);
+    const maxVal = parseFloat(document.getElementById("maxVal").value);
+    const delta = parseFloat(document.getElementById("delta").value);
 
-    let values = calculate_values(expr)
+
+    let values = calculate_values(expr, delta, minVal, maxVal)
     let xValues = values[0]
     let yValues = values[1]
 
@@ -13,9 +17,16 @@ document.getElementById("plotBtn").addEventListener("click", () => {
 
 document.getElementById("heartBtn").addEventListener("click", () => {
     document.getElementById("func").value = "(x^2)^(1/3) + e/3 * sqrt(pi - x^2) * sin(10 * pi * x)"
+    document.getElementById("minVal").value = -4
+    document.getElementById("maxVal").value = 4
+    document.getElementById("delta").value = 0.001
     const expr = document.getElementById("func").value;
+    const minVal = parseFloat(document.getElementById("minVal").value);
+    const maxVal = parseFloat(document.getElementById("maxVal").value);
+    const delta = parseFloat(document.getElementById("delta").value);
 
-    let values = calculate_values(expr, 0.001, -4, 4)
+
+    let values = calculate_values(expr, delta, minVal, maxVal)
     let xValues = values[0]
     let yValues = values[1]
 
@@ -25,6 +36,9 @@ document.getElementById("heartBtn").addEventListener("click", () => {
 document.getElementById("resetBtn").addEventListener("click", () => {
     document.getElementById("func").value = "x"
     const expr = document.getElementById("func").value;
+    document.getElementById("minVal").value = -10
+    document.getElementById("maxVal").value = 10
+    document.getElementById("delta").value = 0.01
 
     let values = calculate_values(expr)
     let xValues = values[0]
